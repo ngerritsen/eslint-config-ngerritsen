f5dfbb71bf161be8d983d16e78403018b7c5ddcb
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-08-27 14:35:57 +0200

    Use js for entry file

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    './eslint/best-practices.json',
+    './eslint/errors.json',
+    './eslint/esnext.json',
+    './eslint/node.json',
+    './eslint/strict-mode.json',
+    './eslint/style.json',
+    './eslint/variables.json'
+  ]
+};
