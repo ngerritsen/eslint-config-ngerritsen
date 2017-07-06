ac8e4fca92358617387b7f11fc5ddce1bced5b01
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-07-06 21:39:54 +0200

    Fix

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = {
     './rules/best-practices.json',
     './rules/errors.json',
     './rules/esnext.json',
-    './rules/filenames.json',
     './rules/import.json',
     './rules/node.json',
     './rules/strict-mode.json',
